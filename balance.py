36dd62df76f322da47a267d779d0e09aa0227a5f
Tyler Albert Louis Deuty <tdeuty@asu.edu>
2018-03-28 16:25:25 -0700

    fixed some errors in balance.py

--- balance.py
+++ balance.py
@@ -6,9 +6,10 @@ import sys
 
 sys.path.insert(0, './app')
 
-from BinanceAPI import BinanceAPI
+from app.BinanceAPI import BinanceAPI
 
-import config
+import app.config as config
+
 
 class Binance:
 
@@ -25,13 +26,14 @@ class Binance:
     def balance(self, asset="BTC"):
         balances = self.client.get_account()
 
-        balances['balances'] = {item['asset']: item for item in balances['balances']}
-        
-        print balances['balances'][asset]['free']
-                 
+        balances['balances'] = {item['asset']
+            : item for item in balances['balances']}
+
+        print(balances['balances'][asset]['free'])
+
     def orders(self, symbol, limit):
         orders = self.client.get_open_orders(symbol, limit)
-        print orders
+        print(orders)
 
     def tickers(self):
         return self.client.get_all_tickers()
@@ -41,23 +43,25 @@ class Binance:
 
     def openorders(self):
         return self.client.get_open_orders()
-        
+
     def profits(self, asset='BTC'):
-        
+
         coins = self.client.get_products()
 
         for coin in coins['data']:
-            
+
             if coin['quoteAsset'] == asset:
-                    
+
                 orders = self.client.get_order_books(coin['symbol'], 5)
-                lastBid = float(orders['bids'][0][0]) #last buy price (bid)
-                lastAsk = float(orders['asks'][0][0]) #last sell price (ask)
-    
-                profit = (lastAsk - lastBid) /  lastBid * 100
-            
-                print('%.2f%% profit : %s (bid:%.8f-ask%.8f)' % (profit, coin['symbol'], lastBid, lastAsk))
-            
+                lastBid = float(orders['bids'][0][0])  # last buy price (bid)
+                lastAsk = float(orders['asks'][0][0])  # last sell price (ask)
+
+                profit = (lastAsk - lastBid) / lastBid * 100
+
+                print('%.2f%% profit : %s (bid:%.8f-ask%.8f)' %
+                      (profit, coin['symbol'], lastBid, lastAsk))
+
+
 try:
 
     m = Binance()
@@ -68,36 +72,36 @@ try:
     print('4 -) Check balance')
     print('Enter option number: Ex: 2')
 
-    option = raw_input()
-    
+    option = input()
+
     if option is '1':
-        
+
         print('Enter symbol: Ex: XVGBTC')
-        
-        symbol = raw_input()
-        
+
+        symbol = input()
+
         # Orders
         print('%s Orders' % (symbol))
         m.orders(symbol, 10)
-    
+
     elif option is '3':
         m.balances()
     elif option is '4':
-        
+
         print('Enter asset: Ex: BTC')
-        
-        symbol = raw_input()
-        
+
+        symbol = input()
+
         print('%s balance' % (symbol))
-        
+
         m.balance(symbol)
     else:
-        
+
         print('Enter Asset (Ex: BTC, ETC, BNB, USDT)')
-        
-        asset = raw_input()
-        
-        print 'Profits scanning...'
+
+        asset = input()
+
+        print('Profits scanning...')
         m.profits(asset)
 
 except 'BinanceAPIException' as e:
